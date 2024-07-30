81b89d143b4cb9c0e68fe6e9a8090e822f752a3d
Aryaman Pathak <165542374+aryamanp13@users.noreply.github.com>
2024-07-30 22:49:37 +0530

    Add files via upload

---
+++ cpp exp4a.cpp
@@ -0,0 +1,31 @@
+//Aryaman Pathak EntcA2 o31 exp4a bitwise operators
+#include <iostream>
+
+using namespace std;
+
+int main() {
+ int a , b, bit_and,bit_or,bit_xor,bit_not_b,bit_not_a;
+ cout<<"Enter the first number: "<<endl;
+ cin>>a;
+ cout<<"Enter the second number: "<<endl;
+ cin>>b;
+ 
+ bit_and = a&b;
+ cout<<bit_and<<endl;
+ bit_or = a|b;
+ cout<<bit_or<<endl;
+ bit_xor = a^b;
+ cout<<bit_xor<<endl;
+ bit_not_a = ~a;
+ cout<<bit_not_a<<endl;
+ bit_not_b = ~b;
+ cout<<bit_not_b<<endl;
+ a = a<<2;
+ cout<<a<<endl;
+ b = b<<2;
+ cout<<b<<endl;
+ a = a>>2;
+ cout<<a<<endl;
+ b = b>>2;
+ cout<<b<<endl;
+ }
---
+++ cpp exp4b.cpp
@@ -0,0 +1,26 @@
+//Aryaman Pathak EntcA2 o31 exp4b multiplication till 10
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i,a,x;
+    cout<<"Enter the number: "<<endl;
+    cin>>a;
+ for(i = 1;i<=10;i++)
+  {x = i*a;
+  cout<<x<<endl;}
+}
+/*output
+Enter the number: 
+16
+16
+32
+48
+64
+80
+96
+112
+128
+144
+160*/
